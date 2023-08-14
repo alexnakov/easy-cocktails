92a76350e22146f316de63c7e1461794d69bd5cb
alexnakov <alex.nakov3@gmail.com>
2023-08-14 17:00:01 +0100

    Getting set up for ingredients submission

--- src/pages/AddSpec.js
+++ src/pages/AddSpec.js
@@ -6,7 +6,8 @@ export default function AddSpec() {
   const [cocktailInfo, setCocktailInfo] = useState({
     name: '',
     method: '',
-    ingredients: '', // TODO LATER [{vodka: 50ml},{passoa: 25ml},{Apple juice: 20ml}]
+    // [{ingridient: vodka, amount: 25, unit: ml},{},]
+    ingredients: '', 
     ice: '',
     garnish: '',
     sideNotes: '',
@@ -14,11 +15,16 @@ export default function AddSpec() {
   })
   
   let storeData = localStorage.getItem('cocktail-list')
-
   if (storeData == 'undefined') { storeData = '' } 
   else {storeData = JSON.parse(storeData)}
-
   const [cocktailList, setCocktailList] = useState(storeData)
+
+  // spirit in the content of the cocktail recipe is not limited to alchohol
+  // perishables should be regarded as spirit so I can use ingredients 
+  // as the collection of ingredients (spirits), amounts and units.
+  const [spirit, setSpirit] = useState('')
+  const [amount, setAmount] = useState('')
+  const [unit, setUnit] = useState('')
 
   const handleUserInput = (event) => {
     const {name, value} = event.target;
@@ -54,8 +60,17 @@ export default function AddSpec() {
         <input onChange={handleUserInput} type="text" id="method" name="method"  value={cocktailInfo.method} /><br />
         <label htmlFor="ice">Ice</label><br />
         <input onChange={handleUserInput} type="text" id="ice" name="ice"  value={cocktailInfo.ice} /><br />
-        <label htmlFor="ingredients">Ingredients and Quantitites</label><br />
-        <input onChange={handleUserInput} type="text" id="ingredients" name="ingredients"  value={cocktailInfo.ingredients}/><br />
+        
+        <fieldset>
+          <label htmlFor="ingredients">Ingredients and Quantitites</label><br />
+          <div style={{display: 'flex', padding: '10px'}}>
+            <input onChange={handleUserInput} type="text" id="ingredient" name="ingredient" value={spirit} style={{margin: '5px'}}/><br />
+            <input onChange={handleUserInput} type="text" id="amount" name="amount" value={amount} style={{margin: '5px'}}/><br />
+            <input onChange={handleUserInput} type="text" id="unit" name="unit" value={unit} style={{margin: '5px'}}/><br />
+            <button style={{margin: '5px'}}>Add ingredient</button>
+          </div>
+        </fieldset>
+      
         <label htmlFor="garnish">Garnish</label><br />
         <input onChange={handleUserInput} type="text" id="garnish" name="garnish"  value={cocktailInfo.garnish} /><br />
         <label htmlFor="sideNotes">Side Notes</label><br />
